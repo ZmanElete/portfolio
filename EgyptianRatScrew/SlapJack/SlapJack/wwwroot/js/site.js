0603c1f0345863a6e4e57198ac979c334d8fa2fc
Zach Wright <zachw@gurutechnologies.net>
2019-12-18 15:03:48 -0700

    Got rid of submodule

---
+++ EgyptianRatScrew/SlapJack/SlapJack/wwwroot/js/site.js
@@ -0,0 +1,22 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+//Intilazation
+
+var pageName = window.location.pathname;
+
+if (pageName == '/') {
+    document.getElementById("rules").style.display = "none";
+
+    document.getElementById("ruleShow").addEventListener("click", displayRules);
+
+    function displayRules() {
+        if (document.getElementById("rules").style.display === "inline") {
+            document.getElementById("rules").style.display = "none";
+            return;
+        }
+        document.getElementById("rules").style.display = "inline";
+    }
+}
